829aa583996dc5f6917da3726ede7e73d9ac0895
Nick Sutterer <apotonick@gmail.com>
2015-06-04 15:55:48 +1000

    require shit.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,10 @@ require 'minitest/autorun'
 
 ENV['RAILS_ENV'] = 'test'
 
-require 'cells/slim'
 require_relative 'dummy/config/environment'
 require "rails/test_help" # adds stuff like @routes, etc.
+
+require "cell/railtie"
+require 'cells/slim'
 
 # Cell::ViewModel.send :include, Cell::Slim
